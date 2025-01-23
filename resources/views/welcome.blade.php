a5f95cfc4ff054e76cec4483228b6aff92b1b67f
vinod-ayckattu <vinodharidas.dev24@gmail.com>
2025-01-23 18:42:47 +0530

    changes made in other-branch

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -98,6 +98,7 @@
 
                                             <p class="mt-4 text-sm/relaxed">
                                                 Testing changes Laravel has wonderful documentation covering every aspect of the framework. Whether you are a newcomer or have prior experience with Laravel, we recommend reading our documentation from beginning to end.
+                                                changes in other-branch
                                             </p>
                                         </div>
                                     </div>
